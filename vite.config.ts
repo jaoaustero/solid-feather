74be93b2f67bc7655fa447c8846fcaf3ec65092f
Jerald Austero <jerald.austero0229@gmail.com>
2025-04-27 09:37:30 +0800

    Fix the build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+	plugins: [solidPlugin()],
+	build: {
+		target: 'esnext',
+		lib: {
+			entry: path.resolve(path.dirname(new URL(import.meta.url).pathname), 'src/index.tsx'),
+			name: 'solid-feather',
+			fileName: ($format) => `solid-feather.${$format}.js`,
+		},
+		rollupOptions: {
+			external: ['solid-js']
+		}
+	},
 });
